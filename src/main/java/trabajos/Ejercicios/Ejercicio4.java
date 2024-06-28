14f0a5e7590a25ec0bfc459572e9b6ebb9679098
Oscar Placeres <racso0213@gmail.com>
2024-06-28 10:06:50 -0500

    Agregando funciones

--- src/main/java/trabajos/Ejercicios/Ejercicio4.java
+++ src/main/java/trabajos/Ejercicios/Ejercicio4.java
@@ -4,22 +4,26 @@ import java.util.Scanner;
 
 public class Ejercicio4 {
 
+
+    public static int grados_cel (int grados){
+        int grados_celsius = (grados - 32) * 5/9;
+        return grados_celsius;
+    }
+
+    public static int grados_fah (int grados){
+        int grados_fahr = (grados * 9/5) + 32;
+        return grados_fahr;
+    }
+
     public static void main(String[] args) {
 
-        int grados_celsius = 0;
-        int grados_fahr = 0;
-        int grados = 0;
+        int grados;
         int seleccion;
-
-
 
         Scanner obtener = new Scanner(System.in);
         System.out.println("Ingresa la temperatura que deseas convertir: ");
         grados = obtener.nextInt();
 
-        grados_celsius = (grados - 32) * 5/9;
-
-        grados_fahr = (grados * 9/5) + 32;
 
         System.out.println(" Selecciona:\n  1. Grados Celsius a fahrenheit\n  2. Grados fahrenheit a celsius" );
         seleccion = obtener.nextInt();
@@ -27,13 +31,13 @@ public class Ejercicio4 {
         switch (seleccion){
 
             case 1:
-                System.out.println( grados + "°C grados celsius, equivalen a: " + grados_fahr + "°F grados fahrenheit");
+                System.out.println( grados + "°C grados celsius, equivalen a: " + grados_fah(grados) + "°F grados fahrenheit.");
                 break;
             case 2:
-                System.out.println( grados + "°F grados fahrenheit, equivalen a: " + grados_celsius + "°C grados celsius");
+                System.out.println( grados + "°F grados fahrenheit, equivalen a: " + grados_cel(grados) + "°C grados celsius.");
                 break;
             default:
-                System.out.println("Opción no valida");
+                System.out.println("Opción no valida.");
 
         }
 
